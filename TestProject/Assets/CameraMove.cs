0ca8d8197df9b7eb6459d7f7a1477c634c18d84e
asikkink <asikkink@hawaii.edu>
2016-03-05 23:10:25 -1000

    Made a test scene with moving camera

---
+++ TestProject/Assets/CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    private Animation anim;
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animation>();
+        StartCoroutine("MoveCamera");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator MoveCamera()
+    {
+        yield return new WaitForSeconds(5);
+        anim.Play("cameramove");
+    }
+}
