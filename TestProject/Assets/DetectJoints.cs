1a702a0b1ceca3df92a21d722aa6246e59576caa
asikkink <asikkink@hawaii.edu>
2016-03-07 07:52:48 -1000

    Started adding Kinect gesture scripts for testing

---
+++ TestProject/Assets/DetectJoints.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class DetectJoints : MonoBehaviour {
+
+    public GameObject BodySourceManager;
+    public JointType TrackJoint;
+    private BodySourceManager bodyManager;
+
+	// Use this for initialization
+	void Start () {
+        if (BodySourceManager == null)
+        {
+
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
