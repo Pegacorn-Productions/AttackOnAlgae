9adb7d46934f425011f76c0508ed4f3c65e25d41
Knight125 <knight2@hawaii.edu>
2016-03-13 21:04:33 -1000

    Now sea urchins pop up when you enter box collider

--- TestProject/Assets/PlayerController.cs
+++ TestProject/Assets/PlayerController.cs
@@ -19,12 +19,19 @@ public class PlayerController : MonoBehaviour {
 	public bool clearCoral4 = false;
 	//holds reference for rigidbody
 	private Rigidbody rb;
+	public GameObject urchin_01;
+	GameObject urchinClone1;
+	GameObject urchinClone2;
+	GameObject urchinClone3;
+	GameObject urchinClone4;
+
 
 
 	//called on the first frame the script is active
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+
 	}
 
 	// Update is called once per frame, before rendering a frame, where most of game will go
@@ -49,15 +56,19 @@ public class PlayerController : MonoBehaviour {
 		//int tag = 0;
 		if (other.CompareTag ("0,1")) {
 			clearCoral1 = true;
+			urchinClone1 = Instantiate (urchin_01, new Vector3(0.5f,0.55f,4.5f), Quaternion.identity) as GameObject;
 		}
 		else if (other.CompareTag ("1,0")) {
 			clearCoral2 = true;
+			urchinClone2 = Instantiate (urchin_01, new Vector3(4.95f,0.55f,0.05f), Quaternion.identity) as GameObject;
 		}
 		if (other.CompareTag ("0,-1")) {
 			clearCoral3 = true;
+			urchinClone3 = Instantiate (urchin_01, new Vector3(0.17f,0.55f,-4.27f), Quaternion.identity) as GameObject;
 		}
 		if (other.CompareTag ("-1,0")) {
 			clearCoral4 = true;
+			urchinClone4 = Instantiate (urchin_01, new Vector3(-5.23f,0.55f,0.74f), Quaternion.identity) as GameObject;
 		}
 		
 			
