5e0f990558882989108c6fb515cc379e41dcb83e
Staticjar <aguaglia@hawaii.edu>
2016-05-01 13:02:12 -1000

    Removed Redundant Items

---
+++ TestProject/Assets/Scripts/animationTests.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationTests : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyUp(KeyCode.I))
+        {
+            anim.SetBool("wave", false);
+            anim.SetBool("move", false);
+        }
+        if(Input.GetKeyUp(KeyCode.M))
+        {
+            anim.SetBool("move", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("wave", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.K))
+        {
+            anim.SetBool("kneel", true);
+            anim.SetBool("stand", false);
+        }
+
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetBool("stand", true);
+            anim.SetBool("kneel", false);
+        }
+	}
+}
