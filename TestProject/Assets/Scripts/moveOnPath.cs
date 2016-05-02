99b7ce02541fb364d9d16d98a7c94fda15f8daf9
Staticjar <aguaglia@hawaii.edu>
2016-05-02 08:57:39 -1000

    commenting
    commented some stuff

--- TestProject/Assets/Scripts/moveOnPath.cs
+++ TestProject/Assets/Scripts/moveOnPath.cs
@@ -5,10 +5,15 @@ public class moveOnPath : MonoBehaviour {
 
     public Paths PathToFollow;
     public int CurrentPointID = 0;
+    //the speed to move along the path
     public float speed;
+    //how close we want to be to a node before aiming for the next one
     public float reachDist = 1f;
+    //rotational speed when we need to switch directions
     public float rotateSpeed = 5f;
     public string pathName;
+
+    //this is the target we want to send a message to
     public GameObject msgTarget;
     public string funcName = "This Is the Function We want to Call";
 
